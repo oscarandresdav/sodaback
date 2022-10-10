1da5bfbb280c547108dbee33fd474e8c4c609552
Oscar Davila <oscar7822002@gmail.com>
2022-10-10 08:41:24 -0500

    Use Route Parameters

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('categories')
 export class CategoriesController {
   @Get()
   findAll() {
     return `This action returns all categories`;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns #${id} category`;
   }
 }
