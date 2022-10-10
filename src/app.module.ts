2c81bd576a0c9ba8d6baef06c993c9c0898a6b28
Oscar Davila <oscar7822002@gmail.com>
2022-10-10 14:23:05 -0500

    Introducing the TypeORM Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [CategoriesModule],
+  imports: [
+    CategoriesModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql', // type of our database
+      host: 'localhost', // database host
+      port: 3306, // database host
+      username: 'nestuser', // username
+      password: 'Nest123.', // user password
+      database: 'soda', // name of our database,
+      autoLoadEntities: true, // models will be loaded automatically
+      synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
