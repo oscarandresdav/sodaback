882368001eb52539a65e6af1f1ab37adfd424bfd
Oscar Davila <oscar7822002@gmail.com>
2022-10-11 16:07:13 -0500

    Introducing the Config Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,13 +8,14 @@ import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql', // type of our database
-      host: 'localhost', // database host
-      port: 3306, // database host
-      username: 'nestuser', // username
-      password: 'Nest123.', // user password
-      database: 'soda', // name of our database,
+      host: process.env.DATABASE_HOST, // database host
+      port: +process.env.DATABASE_PORT, // database host
+      username: process.env.DATABASE_USER, // username
+      password: process.env.DATABASE_PASSWORD, // user password
+      database: process.env.DATABASE_NAME, // name of our database,
       autoLoadEntities: true, // models will be loaded automatically
       synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
     }),
